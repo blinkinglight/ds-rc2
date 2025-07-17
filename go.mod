1e759b3a1c83cdca3145d012617860ffd32025ac
David Coe <gitlab@davycoe.com>
2025-07-17 12:49:56 +0100

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module rc2/bug
+
+go 1.24.4
+
+require (
+	github.com/a-h/templ v0.3.906
+	github.com/go-chi/chi/v5 v5.2.2
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+
+	"rc2/bug/pages"
+
+	"github.com/a-h/templ"
+
+	"github.com/go-chi/chi/v5"
+)
+
+var (
+	//go:embed assets
+	embededFiles embed.FS
+	playerList   = []string{"dave", "jason", "mark", "dan", "sam"}
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	assets, err := fs.Sub(embededFiles, "assets")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.Handle("/*", http.FileServer(http.FS(assets)))
+
+	r.Handle("/qr/*", http.StripPrefix("/qr/", http.FileServer(http.Dir("./qr"))))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
+		templ.Handler(pages.Admin(playerList)).ServeHTTPStreamed(w, r)
+	})
+
+	http.ListenAndServe(":3000", r)
+}
